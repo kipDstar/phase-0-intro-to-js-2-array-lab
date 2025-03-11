105ea85953231df16f9c78773b55b4d6c1243897
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-03-11 07:47:11 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function arrayFunctions() {
+    beforeEach(function () {
+        cats.length = 0;
+        cats.push('Milo', 'Otis', 'Garfield');
+    });
+}
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    return [...cats, "Broom" ];
+}
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
